5c799808457de9354fbd7763cb11fe8c28eb0b4a
AndreevP <phsashp@mail.ru>
2017-09-20 15:51:02 +0300

    BydloKod

--- task_1/task/1_cmd_runner.c
+++ task_1/task/1_cmd_runner.c
@@ -1,5 +1,5 @@
 /*
-  Write a program which allow you to: 
+  Write a program which allow you to:
 - run another programs via command line.
 - get exit codes of terminated programs
 
@@ -9,3 +9,69 @@
    a) if execvp is used, string splitting into "path" and "args" is all you need.
 3. Collect exit codes via waitpid/WEXITSTATUS.
 */
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <malloc.h>
+
+char ** a; // our argv
+
+int StrToArgv(char * s) {
+  int k = 0; // number of words (0 <-> 1)
+  int j = 0, i = 0;
+  a = (char **) malloc(sizeof(char *));
+  while (s[i] != '\n') {
+    a = (char **) realloc(a, (k + 1) * sizeof(char *)); // add memory for new word pointer
+    a[k] = (char *) malloc(100); // memory for every single word
+    j = 0;
+    while (s[i] == ' ')
+      i++;
+    while ((s[i] != ' ') && (s[i] != '\n')) {
+      a[k][j] = s[i];
+      i++;
+      j++;
+    }
+    a[k][j] = '\0';
+    k++;
+  }
+  a = (char **) realloc(a, (k + 1) * sizeof(char *)); // add memory for NULL pointer
+  a[k] = NULL;
+  return k;
+}
+
+void clearargv(int NumberOfWords) {     // make memory free again
+  for (int i = 0; i < NumberOfWords; i++) {
+    free(a[i]);
+  }
+  free(a);
+}
+
+int main() {
+  char str[256] = "";
+  pid_t pid;
+  fgets(str, 255, stdin);
+  int n, status;
+  while(strcmp(str, "quit\n") != 0) {
+    n = StrToArgv(str);
+    switch(pid = fork()) {
+      case 0:                 //child code
+        execv(a[0], a);
+        printf("An error in execv\n"); //this code runs only in case of error
+        exit(2);
+        break;
+      case -1: // error code
+        printf("error, it isn't possible to create new process\n");
+        break;
+      default:
+        waitpid(pid, &status, 0);                //parent code
+        printf("Ret code: %d\n", WEXITSTATUS(status));
+    }
+    clearargv(n);
+    fgets(str, 255, stdin);
+  }
+  return 0;
+}
